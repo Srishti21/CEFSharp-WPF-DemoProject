c4ce5f98952ecc285760a162d8b349348dafa4d3
@srishti.sinha <srishti.sinha@philips.com>
2020-05-29 02:35:17 +0530

    Project to demonstrate CEFSharp in WPF

---
+++ CefSharpWpf/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CefSharpWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void QuickScheduleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChromiumWebBrowser.Address = "https://www.javatpoint.com/";
+        }
+
+        private void GoogleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChromiumWebBrowser2.Address = "https://google.co.in";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Embedding CEFSharp in WPF
+
+This is a starter demo project to demonstrate how to embed chromium-based web browser control in WPF.
